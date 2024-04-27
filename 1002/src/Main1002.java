726caee495fdf4fdfa6988555ee57056d32516ca
CunhaLeticia <leticia.cunha1@hotmail.com>
2024-04-27 19:57:17 -0300

    Cuidado com os espaços xD

--- 1002/src/Main1002.java
+++ 1002/src/Main1002.java
@@ -1,6 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Olá!");
+import java.util.Locale;
+import java.util.Scanner;
+import java.lang.Math;
 
-        }
+public class Main1002 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double n = 3.14159;
+        double raio = sc.nextDouble();
+        double area = n * Math.pow(raio,2);
+
+        System.out.printf("A=%.4f%n" , area);
+
+         }
     }
