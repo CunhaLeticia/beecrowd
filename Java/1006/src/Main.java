e90bd956cce3f6a98bd9f02abbdb9ab870a278c8
CunhaLeticia <leticia.cunha1@hotmail.com>
2024-05-16 19:00:51 -0300

    Novas práticas

--- Java/1006/src/Main.java
+++ Java/1006/src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new  Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+
+        double pesoA = 2;
+        double pesoB = 3;
+        double pesoC = 5;
+
+        double mediaPonderada = (pesoA * a + pesoB * b + pesoC * c) / (pesoA + pesoB + pesoC);
+
+        System.out.printf("MEDIA = %.1f%n", mediaPonderada);
     }
 }
